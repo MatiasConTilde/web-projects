da3045665c20da32f8e59372a77e1a3dabafb2a2
Matías <matias@Matiass-MacBook-Pro.local>
2017-02-21 18:15:10 +0100

    NaWi works

--- nawi/sketch.js
+++ nawi/sketch.js
@@ -1,10 +1,19 @@
-var calculate;
+var velocity,
+    area,
+    density,
+    coefficient,
+    calculate;
 var p;
 
 function setup() {
   //createCanvas(600, 600);
   noCanvas();
 
+  velocity = createSlider(1, 100, 10, 1);
+  area = createSlider(1, 100, 10, 1);
+  density = createSlider(1, 100, 10, 1);
+  coefficient = createSlider(1, 100, 10, 1);
+  calculate = createButton('calculate');
   calculate.mousePressed(calc);
 
   p = createP("");
