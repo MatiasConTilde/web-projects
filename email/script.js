77f3da44cb1bd88cf2ea210e282fdbb6e8668233
ANyanCatFan <saitam.spatz@gmail.com>
2017-03-05 00:34:08 +0100

    Email feedback

--- email/script.js
+++ email/script.js
@@ -1,13 +1,14 @@
-//var socket = io.connect('http://localhost:3000');
-var socket = io.connect('https://email-con-tilde.herokuapp.com');
+var socket = io.connect('http://localhost:3000');
+//var socket = io.connect('https://email-con-tilde.herokuapp.com');
 
 function send() {
   socket.emit('push', {
     name: document.getElementById('name').value,
     email: document.getElementById('email').value
-  })
-};
+  });
+  document.getElementById('result').innerHTML = "Something went wrong";
 
-socket.on('feedback', function(data) {
-  console.log(data.fb);
-});
+  socket.on('feedback', function(data) {
+    document.getElementById('result').innerHTML = data.fb;
+  });
+};
