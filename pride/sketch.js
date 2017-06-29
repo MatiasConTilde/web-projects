ad2ff9e173941e6e3e2c3b1109aeb98d33e475e5
ANyanCatFan <saitam.spatz@gmail.com>
2017-06-29 18:47:38 +0200

    Better pride

--- pride/sketch.js
+++ pride/sketch.js
@@ -1,36 +1,64 @@
 var colors = [];
 var colorIndex = 0;
-var radius = 1;
-var mouse;
+
+var circles = [];
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	noStroke();
 
-	colors = [
-		color(255, 0, 0),
-		color(255, 128, 0),
-		color(255, 255, 0),
-		color(0, 128, 0),
-		color(0, 0, 255),
-		color(160, 0, 192)
-	];
+	switch (location.search.substring(1).replace(/%20/g, ' ').toLowerCase()) {
+		case 'bi':
+			colors = ['#D70270', '#734F96', '#0038A8'];
+			break;
+		case 'pan':
+			colors = ['#FF218C', '#FFD800', '#21B1FF'];
+			break;
+		case 'poly':
+			colors = ['#F61CB9', '#07D569', '#1C92F6'];
+			break;
+		case 'a':
+			colors = ['#000000', '#A3A3A3', '#FFFFFF', '#800080'];
+			break;
+		case 'trans':
+			colors = ['#5BCEFA', '#F5A9B8', '#FFFFFF', '#F5A9B8'];
+			break;
+		default:
+			colors = ['#E40303', '#FF8C00', '#FFED00', '#008026', '#004DFF', '#750787'];
+	}
 
-	mouse = createVector(width / 2, height / 2);
-
+	circles.push(new Circle(createVector(width / 2, height / 2), colorIndex));
 }
 
 function draw() {
-	fill(colors[colorIndex % colors.length]);
-	radius *= 1.25;
-	ellipse(mouse.x, mouse.y, radius, radius);
+	for (var i = 0; i < circles.length; i++) {
+		if (circles[i].radius > (width + height)) {
+			circles.splice(i, 1);
+			continue;
+		}
+		circles[i].update();
+		circles[i].draw();
+	}
 }
 
 function mousePressed() {
-	if (radius > sqrt(sq(width) + sq(height))) {
-		mouse = createVector(mouseX, mouseY);
-		radius = 1;
-		colorIndex++;
+	colorIndex++;
+	colorIndex %= colors.length;
+	circles.push(new Circle(createVector(mouseX, mouseY), colorIndex));
+}
+
+var Circle = function(pos, colorIndex) {
+	this.pos = pos;
+	this.radius = 1;
+	this.colorIndex = colorIndex;
+
+	this.update = function() {
+		this.radius *= 1.1;
+	}
+
+	this.draw = function() {
+		fill(colors[colorIndex]);
+		ellipse(this.pos.x, this.pos.y, this.radius * 2, this.radius * 2);
 	}
 }
 
